3c05409ebe31ec99ca332a18e4793ea7f249b594
彭涛 <pengtaonumber@gmail.com>
2023-04-09 21:59:23 +0800

    测试

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/antd.css';
+
 
 Vue.config.productionTip = false
 
 new Vue({
+  Antd,
   render: h => h(App),
 }).$mount('#app')
